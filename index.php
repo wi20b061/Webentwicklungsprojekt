ee1abdc10f3e144c05d3173675b047067717d400
LAPTOP-0G3JHJ4A\larar <wi19b083@technikum-wien.at>
2022-05-15 15:16:10 +0200

    product sites

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body class="container-fluid pt-3 pb-3">
+    <header>
+        <?php
+            include('nav.php')
+        ?>
+    </header>
+    <main>
+
+    </main>
+</body>
+</html>
