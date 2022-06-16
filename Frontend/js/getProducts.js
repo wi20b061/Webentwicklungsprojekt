dbd9b5cd981a2139db573db66a144204e40e70e3
LAPTOP-0G3JHJ4A\larar <wi19b083@technikum-wien.at>
2022-06-16 16:09:30 +0200

    get product search

--- Frontend/js/getProducts.js
+++ Frontend/js/getProducts.js
@@ -89,7 +89,31 @@ function liveSearch(searchterm) {
             dataType: "json",
             success: function (response) {
                 console.log("search completed")
+                var colCount = 0;
 
+                var rows = '<div class="row ps-2 pe-2">';
+
+                response.forEach(product => {
+                    colCount += 1;
+
+
+                    rows += '<div class="col m-2 border-bottom"><div class="product" id="' + colCount + '"><div class="row"><img class="img-fluid mx-auto d-block " src="' + product.path + '"></div><div class="row"><div class="col"><strong>' + product.name + '</strong><br>' + product.description.substr(0, 20) + '...<br>' + product.price.toFixed(2) + ' €</div></div></div><div  class="row text-end mt-1 mb-1"><button onclick="addToCart(' + colCount + ')" type="button" class="btn text-white btn-sm" style="background-color: #365370; border-color:#365370;">Add to cart <i class="bi bi-basket-fill ms-1"  style=" color: white;"></i></button></div></div>';
+
+                    if (colCount % 4 == 0) {
+
+                        rows += '</div><div class="row ps-2 pe-2">';
+                    }
+                });
+
+                $('#products').append(rows)
+
+                $('.bi').mouseenter(function () {
+
+                })
+                $('.product').click(function () {
+                    console.log(this.id)
+                    window.location.assign('../sites/productDetails.php?productID=' + this.id)
+                })
             },
             error: function (xhr) {
                 console.log("ajax call error")
