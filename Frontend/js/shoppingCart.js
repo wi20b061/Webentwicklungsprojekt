4e207d43628713636982224a0f5e8681814e1a38
LAPTOP-0G3JHJ4A\larar <wi19b083@technikum-wien.at>
2022-06-16 17:10:20 +0200

    user profile, shoppin cart

--- Frontend/js/shoppingCart.js
+++ Frontend/js/shoppingCart.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
    
-    $('#shoppingCart').click(function(){
-        window.location.assign('../sites/shoppingCart.php')
-    })
-
+    loadCart()
+    console.log("hello")
 })  
+
+function loadCart(){
+    $.ajax({
+        type: "GET",
+        url: "../../Backend/ServiceHandler.php?userID=1&request=cart",
+        cache: false,
+        dataType: "json",
+        success: function (response) {
+            console.log('success')
+        },
+        error: function (xhr) {
+            console.log('Request Status: ' + xhr.status + ' Status Text: ' + xhr.statusText + ' ' + xhr.responseText);
+        }
+    })
+}
