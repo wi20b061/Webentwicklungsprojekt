98eec4003f3928c599f4507171295d5858fbf890
LAPTOP-0G3JHJ4A\larar <wi19b083@technikum-wien.at>
2022-06-17 21:38:22 +0200

    deaktivate user, noch nicht fertig

--- Frontend/js/getUser.js
+++ Frontend/js/getUser.js
@@ -16,10 +16,22 @@ function loadUsers(){
 
             var rows = ""
             var colCount
+
+            
             
             response.forEach(user => {
                 colCount += 1;
-                rows += "<div class='row border p-2'>"+user.userID+"<div class='col ps-5 pe-5'>"+user.fname+" "+user.lname+"<br>"+user.username+"<br>"+user.email+"</div><div class='col'>"+user.streetname+"<br>"+user.zip+" "+user.location+"<br>"+user.country+"</div><div class='col'><button class='btn text-white loadOrders' onclick='loadOrders("+ user.userID +")' style='background-color: #365370;' type='link'>Orders</button></div><div id='btn"+user.userID+"' class='col'><button class='btn text-white deactivate' onclick='deactivateUser("+ user.userID +")' style='background-color: #880808;' type='link'>Deaktivate</button></div></div>";
+                
+            
+                if(user.active == 1){
+                    rows += "<div class='row border p-2'>"+user.userID+"<div class='col ps-5 pe-5'>"+user.fname+" "+user.lname+"<br>"+user.username+"<br>"+user.email+"</div><div class='col'>"+user.streetname+"<br>"+user.zip+" "+user.location+"<br>"+user.country+"</div><div class='col'><button class='btn text-white loadOrders' onclick='loadOrders("+ user.userID +")' style='background-color: #365370;' type='link'>Orders</button></div><div id='btn"+user.userID+"' class='col '><button class='btn text-white deactivate' onclick='deactivateUser("+ user.userID +")' style='background-color: #880808;' type='link'>Deaktivate</button></div></div>";
+
+                   
+                }else{
+                    rows += "<div class='row border p-2'>"+user.userID+"<div class='col ps-5 pe-5'>"+user.fname+" "+user.lname+"<br>"+user.username+"<br>"+user.email+"</div><div class='col'>"+user.streetname+"<br>"+user.zip+" "+user.location+"<br>"+user.country+"</div><div class='col'><button class='btn text-white loadOrders' onclick='loadOrders("+ user.userID +")' style='background-color: #365370;' type='link'>Orders</button></div><div id='btn"+user.userID+"' class='col '><button class='btn text-white deactivate'  style='background-color: #880808;' disabled type='link'>Deaktivate</button></div></div>";
+
+                    
+                }
             });
            
             $('#userList').append(rows)
@@ -49,7 +61,7 @@ function deactivateUser(userID){
         success: function (response) { 
             console.log(response)
             $("#btn"+userID).empty()
-            $("#btn"+userID).append("<button class='btn text-white deactivate' onclick='deactivateUser("+ user.userID +")' style='background-color: #880808;' disabled type='link'>Deaktivate</button>")
+            $("#btn"+userID).append("<button class='btn text-white deactivate'  style='background-color: #880808;' disabled type='link'>Deaktivate</button>")
 
         },
         error: function(xhr){
