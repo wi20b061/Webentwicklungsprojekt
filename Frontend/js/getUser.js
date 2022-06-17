c2aa146cac583ac8341f3ef5386964dc0b707cdd
DESKTOP-PU250QC\FelixPC <wi20b031@technikum-wien.at>
2022-06-17 21:24:13 +0200

    getUser Buttons

--- Frontend/js/getUser.js
+++ Frontend/js/getUser.js
@@ -19,7 +19,7 @@ function loadUsers(){
             
             response.forEach(user => {
                 colCount += 1;
-                rows += "<div class='row border p-2'>"+user.userID+"<div class='col ps-5 pe-5'>"+user.fname+" "+user.lname+"<br>"+user.username+"<br>"+user.email+"</div><div class='col'>"+user.streetname+"<br>"+user.zip+" "+user.location+"<br>"+user.country+"</div><div class='col'><button class='btn text-white loadOrders' onclick='loadOrders("+ user.userID +")' style='background-color: #365370;' type='link'>Orders</button></div><div class='col'><button class='btn text-white deactivate' onclick='deactivateUser("+ user.userID +")' style='background-color: #880808;'>Deaktivate</button></div></div>";
+                rows += "<div class='row border p-2'>"+user.userID+"<div class='col ps-5 pe-5'>"+user.fname+" "+user.lname+"<br>"+user.username+"<br>"+user.email+"</div><div class='col'>"+user.streetname+"<br>"+user.zip+" "+user.location+"<br>"+user.country+"</div><div class='col'><button class='btn text-white loadOrders' onclick='loadOrders("+ user.userID +")' style='background-color: #365370;' type='link'>Orders</button></div><div id='btn"+user.userID+"' class='col'><button class='btn text-white deactivate' onclick='deactivateUser("+ user.userID +")' style='background-color: #880808;' type='link'>Deaktivate</button></div></div>";
             });
            
             $('#userList').append(rows)
@@ -40,5 +40,20 @@ function loadOrders(userID){
 
 function deactivateUser(userID){
 
-    window.location.assign('../sites/orders.php?userID=' + userID)
+    $.ajax({
+        type: "POST",
+        url: "../../Backend/ServiceHandler.php?allUsers",
+        cache: false,
+        dataType: "json",
+        data: {request:"user", userRequest:"deactivateUser", userID:"1"},
+        success: function (response) { 
+            console.log(response)
+            $("#btn"+userID).empty()
+            $("#btn"+userID).append("<button class='btn text-white deactivate' onclick='deactivateUser("+ user.userID +")' style='background-color: #880808;' disabled type='link'>Deaktivate</button>")
+
+        },
+        error: function(xhr){
+            console.log('Request Status: ' + xhr.status + ' Status Text: ' + xhr.statusText + ' ' + xhr.responseText);   
+        },
+    })
 }
