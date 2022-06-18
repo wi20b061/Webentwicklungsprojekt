aec52cd085719ccc55976aa86ae4ba208eff219b
DESKTOP-PU250QC\FelixPC <wi20b031@technikum-wien.at>
2022-06-18 19:03:37 +0200

    getOrders advanced

--- Frontend/js/getOrders.js
+++ Frontend/js/getOrders.js
@@ -19,9 +19,9 @@ function loadOrders(userID){
             var rows= ""
 
             response.forEach(saleshead => {
-                rows+= "<table class='table'><thead><tr><th scope='col'>Order ID</th><th scope='col'>Product and ID</th><th scope='col'>Quantity</th><th scope='col'>Image</th></tr></thead><tbody>";
+                rows+= "<table class='table'><thead><tr><th scope='col'>Order ID</th><th scope='col'>Product and ID</th><th scope='col'>Quantity</th><th scope='col'>Image</th><th scope='col'></th></tr></thead><tbody>";
                 saleshead.cartlineList.forEach(cartline =>{
-                    rows+= "<tr><th scope='row'>"+cartline.saleslineID+"</th><td>"+cartline.productID+" "+cartline.productName+"</td><td>"+cartline.quantity+"</td><td><img src='../../Frontend/productpictures/bookshelf.p' alt='Bookshelf'></td></tr>";
+                    rows+= "<tr><th scope='row'>"+cartline.saleslineID+"</th><td>"+cartline.productID+" "+cartline.productName+"</td><td>"+cartline.quantity+"</td><td><img src='../../Frontend/productpictures/bookshelf.p' alt='Bookshelf'></td><td id='btn"+cartline.saleslineID+"'><i class='bi bi-trash' onclick='deleteProduct("+ saleshead.customerID +", "+cartline.productID+")' type='link'></i></td></tr>";
                 })
             });
 
@@ -48,4 +48,26 @@ function getUrlVars(){
         vars[hash[0]] = hash[1];
     }
     return vars;
+}
+
+function deleteProduct(userID, productID){
+//todo
+console.log('jaman')
+
+    $.ajax({
+        type: "POST",
+        url: "../../Backend/ServiceHandler.php",
+        cache: false,
+        dataType: "json",
+        data: {request:"user", userRequest:"deactivateUser", userID:"1"},
+        success: function (response) { 
+            console.log(response)
+            $("#btn"+userID).empty()
+            $("#btn"+userID).append("<button class='btn text-white deactivate' onclick='deactivateUser("+ user.userID +")' style='background-color: #880808;' disabled type='link'>Deaktivate</button>")
+    
+        },
+        error: function(xhr){
+            console.log('Request Status: ' + xhr.status + ' Status Text: ' + xhr.statusText + ' ' + xhr.responseText);   
+        },
+    })
 }
